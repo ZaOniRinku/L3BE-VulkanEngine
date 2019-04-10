c9d3ec49f0810ac4fc22c30fce494df765878674
ZaOniRinku <ZaOniRinku@users.noreply.github.com>
2019-04-10 15:40:17 +0200

    Update compile.sh

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,2 +1,2 @@
-/homelocal/fsi.local/BE/1.1.101.0/x86_64/bin/glslangValidator -V shader.vert
-/homelocal/fsi.local/BE/1.1.101.0/x86_64/bin/glslangValidator -V shader.frag
+/*vulkan_sdk_dir_path*/x86_64/bin/glslangValidator -V shader.vert
+/*vulkan_sdk_dir_path*/x86_64/bin/glslangValidator -V shader.frag
